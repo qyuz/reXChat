c1429ee4ca7dbed39bb10874768e8656a9fe8991
sichain <sichain@mail.ru>
2015-03-07 21:03:53 +0200

    PlaybackController smooth continuous scroll building prepend

--- rex.py
+++ rex.py
@@ -31,29 +31,23 @@ class PlaybackController(xbmc.Monitor):
     def __init__(self):
         self.settings = Settings()
         self.chat = ChatRenderer()
-        self.scrolled = None
+        self.messages = None
+        self.rendered = False
         self.chatRows = 28
         self.prependLines = None
-        self.prependMessages = None
         self.fetchedStart = None
         self.fetchedEnd = None
-        self.renderedStart = None
-        self.renderedEnd = None
         self.twitchAPI = CachedAPI()
         self.rechatService = CachedService()
     def clearChat(self):
-        self.scrolled = None
-        self.renderedStart = None
-        self.renderedEnd = None
+        self.rendered = False
         self.chat.clear()
     def fetchMessages(self):
+        self.rendered = False
         rMessages, start, end = self.rechatService.nextWithRange()
-        if(len(self.prependMessages) > 0):
-            self.fetchedStart = self.toPlayerTime(self.prependMessages[0].receivedAtMs)
-        else:
-            self.fetchedStart = self.toPlayerTime(start)
+        self.fetchedStart = self.toPlayerTime(start)
         self.fetchedEnd = self.toPlayerTime(end)
-        self.messages = self.prependMessages + map(lambda rm: Message(rm, self.streamInfo.recordedAtMs), rMessages)
+        self.messages = map(lambda rm: Message(rm, self.streamInfo.recordedAtMs), rMessages)
         return len(self.messages)
     def getStreamInfo(self):
         self.streamInfo = self.twitchAPI.getStreamInfo(streamId='v3860959')
@@ -61,46 +55,34 @@ class PlaybackController(xbmc.Monitor):
     def isFetched(self, playerTime):
         playerTime = playerTime * 1000
         return playerTime >= self.fetchedStart and playerTime <= self.fetchedEnd
-    def isRendered(self, playerTime):
-        playerTime = playerTime * 1000
-        return playerTime >= self.renderedStart and playerTime <= self.renderedEnd
     def onSettingsChanged(self):
         self.settings['outdated'] = True
     def preparePrepend(self):
-        if(self.scrolled == None):
+        if(self.rendered == False):
             self.prependLines = [''] * self.chatRows
-            self.prependMessages = []
             return
-        linesSeen = []
-        messagesNotSeen = []
-        foundScrolled = False
+        lastLines = []
         i = -1
-        while(len(linesSeen) < self.chatRows):
+        while(len(lastLines) < self.chatRows):
             message = self.messages[i]
             i = i - 1
-            lines = message.getLines()
-            if(message.id == self.scrolled.id or foundScrolled == True):
-                foundScrolled = True
-                linesSeen = lines + linesSeen
-            else:
-                messagesNotSeen.insert(0, message)
-        self.prependLines = linesSeen[-self.chatRows:]
-        self.prependMessages = messagesNotSeen
+            lastLines = message.getLines() + lastLines
+        self.prependLines = lastLines[-self.chatRows:]
     def render(self):
         self.chat.addLines(self.prependLines)
         self.chat.addMessages(self.messages)
-        self.renderedStart = self.fetchedStart
-        self.renderedEnd = self.fetchedEnd
+        self.rendered = True
     def scroll(self, playerTime):
-        playerTime = playerTime * 1000
-        scroll = None
-        for message in self.messages:
-            if (message.absoluteTimeMs > playerTime):
-                if (scroll != None):
-                    self.chat.scrollToMessage(scroll)
-                    self.scrolled = scroll
-                return
-            scroll = message
+        if (self.rendered == True):
+            playerTime = playerTime * 1000
+            scroll = None
+            for message in self.messages:
+                if (message.absoluteTimeMs > playerTime):
+                    if (scroll != None):
+                        self.chat.scrollToMessage(scroll)
+                    return
+                scroll = message
+            self.chat.scrollToMessage(scroll)
     def stop(self):
         self.chat.hide()
     def updateSettings(self):
@@ -114,14 +96,12 @@ playback.getStreamInfo()
 for i in range(100):
     playerTime = xbmc.Player().getTime()
     fetched = playback.isFetched(playerTime)
-    rendered = playback.isRendered(playerTime)
+    playback.scroll(playerTime)
     if(fetched == False):
         playback.preparePrepend()
         playback.fetchMessages()
-    if(fetched == True and rendered == False):
+    if(fetched == True and playback.rendered == False):
         playback.clearChat()
         playback.render()
-    if(rendered == True):
-        playback.scroll(playerTime)
     xbmc.sleep(200)
 playback.stop()
